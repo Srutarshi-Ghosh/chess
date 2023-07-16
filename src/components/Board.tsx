713e58562b17b2076bf92c924d03acaecabf8fcc
Srutarshi Ghosh <srutarshighosh06@gmail.com>
2023-07-17 00:43:33 +0530

    Updated SelectSquare

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -7,13 +7,14 @@ import SquareData from "../types/SquareData";
 import getDefaultSquareColorData from "../functions/GetDefaultSquareColorData";
 import BoardIndex from "../types/BoardIndex";
 import Player from "../constants/Player";
-import getAllValidMoves from "../functions/GetAllValidMoves";
-import getHighlightSquaresData from "../functions/GetHighlightSquaresData";
+import getPieceMoves from "../functions/GetPieceMoves";
+import changeSquareColorOnSelect from "../functions/ChangeSquareColorOnSelect";
+import movePiece from "../functions/MovePiece";
 
 const Board = () => {
-
+	const initialSquareColorData = getDefaultSquareColorData();
+	const [squareColorData, setSquareColorData] = useState<SquareColor[][]>(initialSquareColorData);
 	const [boardData, setBoardData] = useState<SquareData[][]>(initializeBoard());
-	const [squareColorData, setSquareColorData] = useState<SquareColor[][]>(getDefaultSquareColorData());
 	const [selectedPieceInex, setSelectedPieceIndex] = useState<BoardIndex | null>(null);
 	const [shouldRender, setShouldRender] = useState<boolean>(true);
 
@@ -24,19 +25,32 @@ const Board = () => {
 	// const [resetBoard, setResetBoard] = useState<Boolean>(true)
 
 	const selectSquare = (position: BoardIndex, pieceData: SquareData) => {
-		if (!pieceData) return;
+		if (!selectedPieceInex) {
+			if (!pieceData) return;
 
-		const validMoves = getAllValidMoves(boardData, position, pieceData);
-		const highlightedSquareColorData = getHighlightSquaresData(validMoves, squareColorData);
-		setSquareColorData(highlightedSquareColorData);
-		// console.log(validMoves);
-		// console.log(highlightedSquareColorData);
-		// console.log(squareColorData);
-		setShouldRender(prevState => !prevState);
+			const pieceMoves = getPieceMoves(boardData, position, pieceData);
+			changeSquareColorOnSelect(position, pieceMoves, squareColorData, setSquareColorData);
+			setSelectedPieceIndex(position);
+			// setShouldRender(prevState => !prevState);
+		}
+		else {
+			const { posX, posY } = position
+			if(squareColorData[posX][posY] === SquareColor.HIGHLIGHT)
+				movePiece()
+			else if(pieceData) {
+				
+			}
+
+		}
+	};
+
+	const deSelectSquare = () => {
+		if (!selectedPieceInex) return;
+		setSquareColorData(initialSquareColorData);
+		setSelectedPieceIndex(null);
 	};
 
 	const drawBoard = () => {
-
 		return (
 			<div className={styles["board-container"]}>
 				{boardData.map((_, rowIndex) => {
@@ -64,7 +78,14 @@ const Board = () => {
 		);
 	};
 
-	return <div className={styles.board}>{drawBoard()}</div>;
+	return (
+		<div
+			className={styles.board}
+			onClick={deSelectSquare}
+		>
+			{drawBoard()}
+		</div>
+	);
 };
 
 export default Board;
