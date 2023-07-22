4c20fa87d0eed4cee4940a3765f0719b2abb6183
Srutarshi Ghosh <srutarshighosh06@gmail.com>
2023-07-22 11:21:26 +0530

    Create GetMoveNotation.tsx

---
+++ src/functions/GetMoveNotation.tsx
@@ -0,0 +1,22 @@
+import PieceColor from "../constants/PieceColor";
+import BoardIndex from "../types/BoardIndex";
+import SquareData from "../types/SquareData";
+import getSquareName from "./GetSquareName";
+
+const getMoveNotation = (boardData: SquareData[][], oldPosition: BoardIndex, newPosition: BoardIndex): string => {
+	const { posX: oldPosX, posY: oldPosY } = oldPosition;
+	const { posX: newPosX, posY: newPosY } = newPosition;
+	const pieceData = boardData[oldPosX][oldPosY];
+	if (!pieceData) return "";
+
+	const oldPositionSquareName = getSquareName(oldPosition);
+	const newPositionSquareName = getSquareName(newPosition);
+
+	var moveNotation = `${PieceColor[pieceData.pieceColor]} - ${oldPositionSquareName} ${newPositionSquareName}`;
+	const capturedPiece = boardData[newPosX][newPosY];
+	if (capturedPiece) 
+		moveNotation += ` | ${capturedPiece.pieceType} takes ${capturedPiece.pieceType}`;
+
+	return moveNotation;
+};
+export default getMoveNotation;
